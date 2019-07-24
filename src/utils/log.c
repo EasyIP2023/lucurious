b8c9b7c995552a340cf5f8e9e1fd45decc7b98e7
EasyIP2023 <vdavis2495@gmail.com>
2019-07-23 20:03:50 -0500

    add compile time DEBUG macro

--- src/utils/log.c
+++ src/utils/log.c
@@ -7,52 +7,43 @@
 static const char *term_colors[] = {
 	[WLU_NONE]   	= "",
 	[WLU_SUCCESS] = "\x1B[32;1m",
-	[WLU_DANGER ] = "\x1B[31;1m",
-	[WLU_INFO   ] = "\x1B[30;1m",
+	[WLU_DANGER] 	= "\x1B[31;1m",
+	[WLU_INFO] 		= "\x1B[30;1m",
 	[WLU_WARNING] = "\x1B[33;1m",
 	[WLU_RESET]   = "\x1b[0m",
 };
 
 void _wlu_log_me(wlu_log_type type, const char *fmt, ...) {
 	char buffer[26];
-  va_list args; /* type that holds variable arguments */
+	va_list args; /* type that holds variable arguments */
 
-  /* create message time stamp */
+	/* create message time stamp */
 	struct tm result;
 	time_t t = time(NULL);
 	struct tm *tm_info = localtime_r(&t, &result);
 
-  /* generate time */
-  strftime(buffer, sizeof(buffer), "%F %T - ", tm_info);
+	/* generate time */
+	strftime(buffer, sizeof(buffer), "%F %T - ", tm_info);
 	fprintf(stdout, "%s", buffer);
 
 	unsigned c = (type < WLU_MAX_LOG_ENUM) ? type : WLU_MAX_LOG_ENUM - 1;
 
 	fprintf(stdout, "%s", term_colors[c]);
 
-  va_start(args, fmt);
-  vfprintf(stdout, fmt, args);
-  va_end(args);
+	va_start(args, fmt);
+	vfprintf(stdout, fmt, args);
+	va_end(args);
 
 	/* Reset terminal color */
 	fprintf(stdout, "%s\n", term_colors[WLU_RESET]);
-	fflush(stdout);
 }
 
 const char *_wlu_strip_path(const char *filepath) {
-	char cwd[PATH_MAX];
-	if (getcwd(cwd, sizeof(cwd)) != NULL) {
-		static int srclen = sizeof(cwd);
-		if (strstr(filepath, cwd) == filepath) {
-			filepath += srclen;
-		} else if (*filepath == '.') {
-			while (*filepath == '.' || *filepath == '/') {
-				++filepath;
-			}
-		}
-	} else {
+	if (*filepath == '.')
+		while (*filepath == '.' || *filepath == '/')
+			++filepath;
+	else
 		return NULL;
-	}
 
 	return filepath;
 }
